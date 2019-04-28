bf7dedacf198f652ab03915756ed57c1242ea3ac
Karthik Sankaran <contact@karthiksankaran.com>
2019-04-28 12:08:19 -0400

    Set up VueLayers

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Vue from 'vue'
+import VueLayers from 'vuelayers'
+import 'vuelayers/lib/style.css' // needs css-loader
+
+Vue.use(VueLayers)
 Vue.config.productionTip = false
 
 new Vue({
